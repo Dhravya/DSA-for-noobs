e4c203bc707a28b057059f7f675a4246223224b3
Dhravya <dhravyashah@gmail.com>
2022-01-09 23:59:13 +0530

    Added reverse algorithm and more explanation in README file

--- README.md
+++ README.md
@@ -17,3 +17,35 @@ Removes element from the top of the stack
 Returns the element at the top of the stack
 
 Size and is_empty are just helper functions 
+
+### Stuff Done with stack:
+- Checking if a bracket string is balanced
+    Explanation:
+    - Add all brackets in the stack one by one
+    - on reaching a closing bracket, check if the top of the stack is a matching bracket
+    - if it is, pop the top of the stack and check the next bracket
+    - rinse and repeat 
+    - if the stack is empty, the string is balanced
+    - otherwise, the string is not balanced in the end
+
+- Reverse algorithm
+    Explanation:
+    - Add all characters in the string to the stack
+    - make a new string - which will hold the return value
+    - pop the top of the stack until its empty and add it to the string
+    - Voila! the string has been reversed
+
+- Int to binary conversion
+    Explanation:
+    - Divide the integer by 2 and add the remainder to the stack
+    - the quotient obtained from the division is what we'll use to get the next remainder
+    - Keep repeating until the quotient is 0
+    - Reverse the stack by popping its values to obtain the binary string
+    ```
+        ___________________________
+        |Quotient: 2 Remainder: 1  | - remainder added (Current stack: 1)
+        |Quotient: 1 Remainder: 0  | - remainder added (Current stack: 10)
+        |Quotient: 0 Remainder: 1  | - remainder added (Current stack: 101)
+        ____________________________
+        Answer will be the reverse of this stack : 101
+        ```
---
+++ int_to_binary.py
@@ -0,0 +1,63 @@
+from stack import Stack
+import math
+
+
+def convert_int_to_bin(dec_num):
+    """Convert an integer to binary.
+
+    How does it work?
+    
+    Its important to know how the logic of converting int to binary works.
+    The logic is simple:
+    - If the number is 0, return 0
+    - If the number is 1, return 1
+    - If the number is greater than 1, divide the number by 2 and add the remainder to the stack 
+        The quotient obtained from the division is used to determine the next bit.
+    - Repeat the process until the number is 0.
+
+    So, Lets say we have the number:
+    - 5
+    - 5 / 2 = 2 with remainder 1
+    - 2 / 2 = 1 with remainder 0
+    - 1 / 2 = 0 with remainder 1
+
+    So, the stack will look like this:
+    - 1
+    - 0
+    - 1
+
+    So, the binary representation of 5 is:
+    - 1
+    - 0
+    - 1
+    (Its the reverse of the stack obtained)
+
+    Put another way, all the remainders are added to the stack, and quotients 
+        are used to determine the next bit.
+    after the stack is made, the stack is reversed and the binary representation is returned.
+
+    To have a better understanding, change `n` below to any number and run the program.
+    """
+
+    stack = Stack()
+    print("___________________________")
+    while True:
+        quotient = math.floor(dec_num / 2)
+        remainder = dec_num % 2
+        stack.push(remainder)
+        dec_num = quotient
+        string = "|Quotient: "+ str(quotient)+ " Remainder: "+ str(remainder)
+        al = " "*(27-len(string)) + "|"
+        print(string +al)
+        if quotient == 0:
+            break
+    print("____________________________")
+    print("Now, we will reverse the stack and obtain the binary representation of the number.")
+    bin_num = ""
+    while not stack.is_empty():
+        bin_num += str(stack.pop())
+    return bin_num
+
+n = 5
+ans = convert_int_to_bin(n)
+print(f"\nCalculated Value: {ans} ; Expected Value: {bin(n)[2:]} \n       Correct answer? { ans== bin(n)[2:]}")
